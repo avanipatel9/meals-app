5dbedb6128a143dee5bbfea9986cfca8246dbf46
Avani Patel <anpatel36@yahoo.in>
2021-05-19 01:46:34 +0530

    Adding Screens

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
@@ -8,7 +8,3 @@ export default function App() {
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  
-});
