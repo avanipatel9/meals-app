3493da4869659369744340a7810bd22d1d01b4f4
Avani Patel <anpatel36@yahoo.in>
2021-05-28 20:13:44 +0530

    Redux Data and Navigation Options

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ScrollView, Image, View, Text, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
@@ -20,6 +20,11 @@ const MealDetailScreen = props => {
 
     const selectedMeal = availableMeals.find(meal => meal.id === mealId);
 
+    // useEffect(()=> {
+    //     props.navigation.setParams({mealTitle: selectedMeal.title});
+    // }, [selectedMeal]);
+    
+
     return (
         <ScrollView>
             <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
@@ -39,10 +44,11 @@ const MealDetailScreen = props => {
 MealDetailScreen.navigationOptions = (navigationData) => {
 
     const mealId = navigationData.navigation.getParam('mealId');
-    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+    const mealTitle = navigationData.navigation.getParam('mealTitle');
+    //const selectedMeal = MEALS.find(meal => meal.id === mealId);
 
     return {
-        headerTitle: selectedMeal.title,
+        headerTitle: mealTitle,
         headerRight: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
             <Item title='Favorite' iconName='ios-star' onPress={() => {
                 console.log('Mark as Favorite!');
